6036399ae1fac52289d7c088d30f496e6553eca4
roy <royyana@gmail.com>
2023-10-25 23:07:28 +0700

    jkernel for golang

--- environment/script/run-go.sh
+++ environment/script/run-go.sh
@@ -1,2 +1,2 @@
 sudo chmod -R a+rwx /home/jovyan
-/usr/bin/jupyter-notebook --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''  --NotebookApp.ip='0.0.0.0' --Notebook.autoreload=True --NotebookApp.notebook_dir=/home/jovyan/work --allow-root
+jupyter-lab --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''  --NotebookApp.ip='0.0.0.0' --Notebook.autoreload=True --NotebookApp.notebook_dir=/home/jovyan/work --allow-root
