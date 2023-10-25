7f1de7c85f7c19a5eb142cdccfb8943698530c1a
roy <royyana@gmail.com>
2023-10-26 00:38:29 +0700

    delete unnecessary

--- environment/script/run-go.sh
+++
@@ -1,2 +0,0 @@
-sudo chmod -R a+rwx /home/jovyan
-jupyter-lab --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''  --NotebookApp.ip='0.0.0.0' --Notebook.autoreload=True --NotebookApp.notebook_dir=/home/jovyan/work --allow-root
